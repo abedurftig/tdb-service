b5fd825c65e5d7857f3e71688ff8cb309c4c2585
arnebeduerftig <arne.beduerftig@friday.de>
2017-11-28 11:49:06 +0100

    Added project test and added out folder to gitignore

--- src/test/java/org/testdashboard/model/ProjectTest.java
+++ src/test/java/org/testdashboard/model/ProjectTest.java
@@ -1,10 +1,27 @@
 package org.testdashboard.model;
 
+import org.h2.jdbc.JdbcSQLException;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testdashboard.Application;
 
+import static org.junit.Assert.fail;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class ProjectTest {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    ProjectRepository projectRepository;
 
     @Test
     public void constructorWithoutExternalIdParameterShouldMatchName() {
@@ -19,6 +36,22 @@ public class ProjectTest {
 
         Project project = new Project(null,"Test Project", "External ID");
         Assert.assertThat(project.getExternalId(), Matchers.is("External ID"));
+
+    }
+
+    @Test
+    public void externalIdUniqueConstraint() {
+
+        Account account = accountRepository.save(new Account());
+        Project project = projectRepository.save(new Project(account, "Test One", "123AB"));
+
+        try {
+            projectRepository.save(new Project(account, "Test Two", "123AB"));
+            fail("Should throw unique constraint exception");
+        } catch (DataIntegrityViolationException e) {}
+
+        projectRepository.delete(project);
+        accountRepository.delete(account);
 
     }
 
