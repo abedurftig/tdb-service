9d66394042e4bf579aa12dacb3fe24fb6b0d871a
Arne Bedurftig <dasnervtdoch@googlemail.com>
2017-12-10 01:42:14 +0100

    Moved stuff into folders.

--- upload/upload.sh
+++ upload/upload.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-baseDir=${PWD}
-printf "$baseDir"
-for filename in $baseDir/build/test-results/test/*.xml; do
+baseDir=${PWD}/../build/test-results/test
+for filename in $baseDir/*.xml; do
   curl \
     -F "externalProjectId=Project One" \
     -F "externalTestRunId=TestRun123" \
     -F "file=@$filename" \
     localhost:8080/api/upload-junit4-xml
-  printf "\n\nUploaded $filename\n"
 done
