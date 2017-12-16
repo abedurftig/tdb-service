b48aba7a21413522a4182e2eef40eb8ac11515f2
Arne Bedurftig <dasnervtdoch@googlemail.com>
2017-12-16 17:59:27 +0100

    Connecting to postgress from DATABASE_URL.

---
+++ src/main/java/org/tdb/config/PostgresDBConfig.java
@@ -0,0 +1,38 @@
+package org.tdb.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Profile("pg-standalone")
+public class PostgresDBConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PostgresDBConfig.class);
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        LOGGER.info("Connecting to DB {} as user {}", dbUrl, username);
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+
+    }
+
+}
