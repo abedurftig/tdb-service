0486f6729c648765b77c9358b7336b72b41f3994
arnebeduerftig <arne.beduerftig@friday.de>
2018-01-12 21:41:33 +0100

    Added entities Dashboard and DashboardProject.

---
+++ src/main/java/org/tdb/model/Dashboard.java
@@ -0,0 +1,34 @@
+package org.tdb.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "dashboard")
+public class Dashboard extends BaseEntity {
+
+    @ManyToOne
+    private Account account;
+
+    @OneToMany(mappedBy = "dashboard",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER)
+    private Set<DashboardProject> projects = new HashSet<>();
+
+    public Dashboard(String name, Account account) {
+        super(name);
+        this.account = account;
+    }
+
+    public Dashboard() {}
+
+    public void addProject(Project project) {
+        projects.add(new DashboardProject(this, project));
+    }
+
+    public void removeProject(Project project) {
+        projects.remove(project);
+    }
+
+}
---
+++ src/main/java/org/tdb/model/DashboardRepository.java
@@ -0,0 +1,6 @@
+package org.tdb.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DashboardRepository extends JpaRepository<Dashboard, Long> {
+}
