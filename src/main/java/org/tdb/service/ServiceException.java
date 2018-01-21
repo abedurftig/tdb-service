ce002f65af7a0247b1b321c8bc965a9e6913af68
Arne Bedurftig <dasnervtdoch@googlemail.com>
2018-01-21 15:19:50 +0100

    Refactored service exception.

---
+++ src/main/java/org/tdb/service/ServiceException.java
@@ -0,0 +1,17 @@
+package org.tdb.service;
+
+public class ServiceException extends Exception {
+
+    private WithErrorCode errorCode;
+
+    public ServiceException(WithErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public WithErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public interface WithErrorCode {}
+
+}
